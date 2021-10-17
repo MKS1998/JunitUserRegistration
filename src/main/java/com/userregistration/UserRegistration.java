7019dbc21e3f7fde706e174e441606cf2151db15
MKS1998 <nksayyad2900@outlook.com>
2021-10-17 14:45:14 +0530

    Added : Password Should Have Atleast 1 Upper Case

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -24,16 +24,26 @@ public class UserRegistration {
 		Matcher matcher = pattern.matcher(email);
 		return matcher.matches();
 	}
-	 public boolean phoneNumber (String phoneNumber){
-         String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
-         Pattern pattern = Pattern.compile(regex);
-         Matcher matcher = pattern.matcher(phoneNumber);
-         return matcher.matches();
-     }
-	 public boolean password(String password) {
-	        String regex = "^[0-9a-zA-Z!,@#$&*().]{8,}$";
-	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(password);
-	        return matcher.matches();
-	    }
+
+	public boolean phoneNumber(String phoneNumber) {
+		String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(phoneNumber);
+		return matcher.matches();
+	}
+
+	public boolean password(String password) {
+		String regex = "^[0-9a-zA-Z!,@#$&*().]{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+	}
+
+	public boolean passwordRule2(String password) {
+		String regex = "^(?=.*[A-Z]){1}(?=.*[a-z]).{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+
+	}
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -17,49 +17,66 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.firstName("mohin");
 		Assert.assertEquals(false, result);
 	}
+
 	@Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.lastName("Sayyad");
-        Assert.assertEquals(true, result);
-    }
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Sayyad");
+		Assert.assertEquals(true, result);
+	}
 
-    @Test
-    public void givenLastName_WhenNotProper_ShouldReturnFalse() {
-        boolean result = userRegistration.lastName("sayyad");
-        Assert.assertEquals(false, result);
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("sayyad");
+		Assert.assertEquals(false, result);
 
-    }
-    @Test
-    public void givenEmail_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.email("mohins98@gmail.com");
-        Assert.assertEquals(true, result);
-    }
+	}
 
-    @Test
-    public void givenEmail_WhenNotProper_ShouldReturnFalse() {
-        boolean result = userRegistration.email("mohin&s98@gmail.com");
-        Assert.assertEquals(false, result);
-    }
-    @Test
-    public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.phoneNumber("91 7498656661");
-        Assert.assertEquals(true, result);
-    }
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.email("mohins98@gmail.com");
+		Assert.assertEquals(true, result);
+	}
 
-    @Test
-    public void givenPhoneNumber_WhenNotProper_ShouldReturnFlase() {
-        boolean result = userRegistration.phoneNumber("917498656661");
-        Assert.assertEquals(false, result);
-    }
-    @Test
-    public void givenPassword_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.password("mohin@123");
-        Assert.assertEquals(true, result);
-    }
-    @Test
-    public void givenPassword_WhenNotProper_ShouldReturnFalse() {
-        boolean result = userRegistration.password("ms@123");
-        Assert.assertEquals(false, result);
-    }
+	@Test
+	public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.email("mohin&s98@gmail.com");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.phoneNumber("91 7498656661");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenNotProper_ShouldReturnFlase() {
+		boolean result = userRegistration.phoneNumber("917498656661");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenPassword_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.password("mohin@123");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPassword_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.password("ms@123");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenPasswordRule2_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.passwordRule2("passWord@123");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPasswordRule2_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.passwordRule2("psw@");
+		Assert.assertEquals(false, result);
+	}
 
 }
